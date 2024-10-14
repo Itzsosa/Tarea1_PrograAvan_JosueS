b744bb441658f8251750ae075878ae5893163d3f
Josue Sosa <65882287+Itzsosa@users.noreply.github.com>
2024-10-14 10:30:36 -0600

    Created Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid',
+  integrations: [mdx()],
+  adapter: node({
+    mode: 'standalone'
+  })
+});
---
+++ src/data/blog-posts/test1.md
@@ -0,0 +1,11 @@
+---
+title: This is a test
+publishDate: 7 Oct 2024
+description: This is the second post of the blog
+---
+
+![Illustration of woman using a meditation app](/assets/blog/casual-life-3d-meditation-crystal.webp)
+
+This post intentionally left blank.
+
+Write what you want.
